16c569231dca5bca6e78c43d385269a3edae1c9e
Kian Masri <kianmasri@gmail.com>
2025-12-24 16:05:07 -0700

    add expected values to collections

--- PerformanceCalculatorGUI/Screens/Collections/Collection.cs
+++ PerformanceCalculatorGUI/Screens/Collections/Collection.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+
 namespace PerformanceCalculatorGUI.Screens.Collections
 {
     public class Collection
@@ -8,5 +10,12 @@ namespace PerformanceCalculatorGUI.Screens.Collections
         public required string FileName { get; set; }
         public required string Name { get; set; }
         public required long[] Scores { get; set; }
+        public Dictionary<long, ExpectedPerformanceValues> ExpectedPerformance { get; set; } = new Dictionary<long, ExpectedPerformanceValues>();
+    }
+
+    public class ExpectedPerformanceValues
+    {
+        public double? Total { get; set; }
+        public Dictionary<string, double> Skills { get; set; } = new Dictionary<string, double>();
     }
 }
